37549163b765da4631d0c64aefd08dfa1919c63c
Aljaž Marn <aljazmarn@Aljazs-MacBook-Pro.local>
2025-11-03 19:04:28 +0100

    Popravek

--- 03-11-2025/1del.py
+++ 03-11-2025/1del.py
@@ -1,8 +1,12 @@
 # Napiši program, ki uporablja modul modul1.py in analiziraj besedilo. Besedilo je v datoteki
 # besedilo.txt in vam ga ni treba spreminjati. Funkcija, ki besedilo odpira vam ni treba napisati.
 
-besedilo = open("besedilo.txt", "r", encoding="utf-8", errors="replace")
+besedilo = ""
+with open("besedilo.txt", "r", encoding="utf-8") as f:
+    for l in f:
+        besedilo += l
 
+print(besedilo)
 # Napišite program, ki pove: 
 # - najpogostejšo besedo v stavku,
 # - najpogostejšo črko, 
