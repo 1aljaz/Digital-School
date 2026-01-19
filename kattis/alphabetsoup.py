cc9a0342fde8ea48080e0b3cd7ee2b740385a255
1aljaz <azo.marn@gmail.com>
2026-01-19 17:24:04 +0100

    removing kattis

--- kattis/alphabetsoup.py
+++
@@ -1,9 +0,0 @@
-# https://open.kattis.com/problems/alphabetsoup
-
-s = input()
-st = set(s)
-alph = set("ABCDEFGHIJKLMNOPRSTUWVZQYX")
-if len(st) == 26:
-    print("ALPHABET SOUP!")
-else:
-    print(''.join(sorted(c for c in list(alph - st))))
